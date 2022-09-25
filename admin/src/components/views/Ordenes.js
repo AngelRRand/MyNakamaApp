725b6fc7524998f7e4296bc29d57cdc5aa0788ec
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-25 14:07:08 -0300

    functionSnap

--- admin/src/components/views/Ordenes.js
+++ admin/src/components/views/Ordenes.js
@@ -1,6 +1,24 @@
 import React, { useEffect, useState, useContext } from 'react'
-
+import {FirebaseContext} from '../../firebaseData';
 const Ordenes = () => {
+
+  const { firebase } =  useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerOrdenes = () =>{
+      firebase.db.collection('ordenes').where('completado', '==', false).onSnapshot(manejarSnapshot)
+    }
+    obtenerOrdenes()
+  }, []);
+
+  function manejarSnapshot(snap){
+    const ordenes = snap.docs.map(doc =>{
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+  }
   return (
     <>
         <h1 className='text-3xl font-light text-white'>Ordenes</h1>
