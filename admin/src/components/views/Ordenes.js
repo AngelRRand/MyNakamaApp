1cd8063bae1ebe0953c783c9cee304f8d6c0a465
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-26 11:55:53 -0300

    a

--- admin/src/components/views/Ordenes.js
+++ admin/src/components/views/Ordenes.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState, useContext } from 'react'
-import {FirebaseContext} from '../../firebaseData';
+import { FirebaseContext } from '../../firebaseData';
 import Orden from '../ui/Orden';
 const Ordenes = () => {
 
-  const { firebase } =  useContext(FirebaseContext);
+  const { firebase } = useContext(FirebaseContext);
 
   const [ordenes, setOrdenes] = useState([]);
 
   useEffect(() => {
-    const obtenerOrdenes = () =>{
+    const obtenerOrdenes = () => {
       firebase.db.collection('ordenes').where('completado', '==', false).onSnapshot(manejarSnapshot)
     }
     obtenerOrdenes()
   }, []);
 
-  function manejarSnapshot(snap){
-    const ordenes = snap.docs.map(doc =>{
+  function manejarSnapshot(snap) {
+    const ordenes = snap.docs.map(doc => {
       return {
         id: doc.id,
         ...doc.data()
@@ -26,13 +26,20 @@ const Ordenes = () => {
   console.log(ordenes)
   return (
     <>
-        <h1 className='text-3xl font-light text-white'>Ordenes</h1>
+      <h1 className='text-3xl font-light text-white'>Ordenes</h1>
+      <div className='sm:flex sm:flex-wrap -mx-3'>
+
         {
-          ordenes.map(orden =>{
-            
+          ordenes.map(orden => {
+            <Orden
+              key={orden.id}
+              orden={orden}
+            />
           })
-          
+
         }
+        
+      </div>
     </>
   )
 }
