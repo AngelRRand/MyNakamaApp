76e093ee4f1538e763d4760409cde7786eafc7fa
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-26 11:58:04 -0300

    component

--- admin/src/components/ui/Orden.js
+++ admin/src/components/ui/Orden.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Orden = ({orden}) => {
   return (
-    <div>Orden</div>
+    <div className='sm:w-1/2 lg:w-1/3 px-2 mb-4'>
+        <div className='p-3 shadow-md bg-slate-800'>
+            
+        </div>
+    </div>
   )
 }
 
