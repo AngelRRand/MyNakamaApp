868a992e267bb26dea7328327158354fdfcbf791
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-26 12:22:10 -0300

    stylesHtmlOrder

--- admin/src/components/ui/Orden.js
+++ admin/src/components/ui/Orden.js
@@ -3,24 +3,30 @@ import React from 'react'
 const Orden = ({orden}) => {
     console.log((orden))
   return (
-    <div className='sm:w-1/2 lg:w-1/3 px-2 mb-4'>
+    <div className='sm:w-1/2 lg:w-1/3 px-2 mb-4 p-5'>
         <div className='p-3 shadow-md bg-slate-800'>
-            <h1 className='text-pink-700 text-lg font-bold'>{orden.id}</h1>
             {orden.orden.map(plato =>(
                 <p className='text-pink-400'>{plato.cantidad} {plato.nombre}</p>
             ))}
             <p className='text-pink-700 font-bold'>Total a pagar: $ {orden.total}</p>
             {orden.tiempoentrega === 0 && (
-                <div className='mb-4'>
+                <div className='mb-4 mt-4'>
                     <label className='block text-white text-sm font-bold mb-2'>
                         Tiempo de entrega
                     </label>
                     <input
                         type='number'
-                        className='shadow apparence-none border rounded w-full py-2 px-3 text-pink-600 leading-tight focus:outline-none focus:shadow-outline'
+                        className='shadow apparence-none border rounded w-full py-2 px-3 text-pink-600 leading-tight focus:outline-none focus:shadow-outline bg-slate-900'
                         min='1'
                         max='20'
+                        placeholder='20'
                     />
+                    <button
+                        type='submit'
+                        className='mb-1 p-2 mt-3 text-white uppercase font-bold bg-pink-700 hover:bg-pink-900 inline-block'
+                    >
+                        Definir Tiempo
+                    </button>
                 </div>
             )}
         </div>
