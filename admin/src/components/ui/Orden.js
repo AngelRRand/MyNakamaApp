7d75caaee9226081ac3f782e8fabccf92d78575f
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-26 11:59:35 -0300

    prueba

--- admin/src/components/ui/Orden.js
+++ admin/src/components/ui/Orden.js
@@ -4,7 +4,7 @@ const Orden = ({orden}) => {
   return (
     <div className='sm:w-1/2 lg:w-1/3 px-2 mb-4'>
         <div className='p-3 shadow-md bg-slate-800'>
-            
+            <h1 className='text-pink-700 text-lg font-bold'>{orden.nombre}</h1>
         </div>
     </div>
   )
